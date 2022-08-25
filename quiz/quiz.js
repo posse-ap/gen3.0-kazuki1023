40348218d8efb2aa8338de788cfce171c46c56d6
kazuki iwagi <k.keio12569@gmail.com>
2022-08-25 15:42:53 +0900

    とりあえずエラー消えないから、そこのfor文のコメントアウトした。そこは放置して正解、不正解がしっかり正しく表示されるようにする

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -6,6 +6,15 @@
 // https://techmemo.biz/javascript/click-toggle-class/
 
 // とりあえず、以下にhtmlでjsを書くをやってみて、うまく行ったらそれをコピーするという形を取りたいとおもう
+
+// const quizSet= [
+//     問題文,
+//     選択肢,
+//     正解、不正解の選択肢
+//     画像のurlをとってきた
+//     ”正解！”の文字をとってきた。これで解答に”正解！”って出したい
+//     "不正解..."の文字をとってきた。これで解答に”不正解！”って出したい。これ上のを使えばこれなくしてもできそうだけど、やり方わからんから、とりま放置
+// ]
 const quizSet = [
   [
     "日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
@@ -22,11 +31,6 @@ const quizSet = [
     "img-quiz01.png",
     1,
     0,
-    [
-      "question1_answer_incorrect",
-      "question1_answer_correct",
-      "question1_answer_incorrect"
-    ]
   ],
   [
     "既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？",
@@ -43,11 +47,6 @@ const quizSet = [
     "img-quiz02.png",
     2,
     0,
-    [
-      "question2_answer_incorrect",
-      "question2_answer_incorrect",
-      "question2_answer_correct"
-    ]
   ],
   [
     "IoTとは何の略でしょう？",
@@ -64,11 +63,6 @@ const quizSet = [
     "img-quiz03.png",
     0,
     1,
-    [
-      "question3_answer_correct",
-      "question3_answer_incorrect",
-      "question3_answer_incorrect"
-    ]
   ],
   [
     "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
@@ -85,11 +79,6 @@ const quizSet = [
     "img-quiz04.png",
     0,
     1,
-    [
-      "question4_answer_correct",
-      "question4_answer_incorrect",
-      "question4_answer_incorrect"
-    ]
   ],
   [
     "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
@@ -107,11 +96,6 @@ const quizSet = [
     //  ここに , を入れ忘れて画像が読み込めなかった！！  注意！！！！！
     0,
     1,
-    [
-      "question5_answer_correct",
-      "question5_answer_incorrect",
-      "question5_answer_incorrect"
-    ]
   ],
   [
     "先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？",
@@ -128,13 +112,92 @@ const quizSet = [
     "img-quiz06.png",
     1,
     0,
-    [
-      "question6_answer_incorrect",
-      "question6_answer_correct",
-      "question6_answer_incorrect"
-    ]
   ],
   ]
+  // const quizAnswer = [
+  //  [
+  //   js_answer[問題番号]_boxにクラスをつける
+  //  ]
+  //  [
+  //   js_answer
+  //  ]
+  // ]
+  const quizAnswer = [
+    [
+      [
+        "question1_answer_incorrect",
+        "question1_answer_correct",
+        "question1_answer_incorrect"
+      ],
+      [
+        "question1_answer_incorrect_title",
+        "question1_answer_correct_title",
+        "question1_answer_incorrect-title"
+      ]
+    ],
+    [
+      [
+        "question2_answer_incorrect",
+        "question2_answer_correct",
+        "question2_answer_incorrect"
+      ],
+      [
+        "question2_answer_incorrect_title",
+        "question2_answer_correct_title",
+        "question2_answer_incorrect-title"
+      ]
+    ],
+    [
+      [
+        "question3_answer_incorrect",
+        "question3_answer_correct",
+        "question3_answer_incorrect"
+      ],
+      [
+        "question3_answer_incorrect_title",
+        "question3_answer_correct_title",
+        "question3_answer_incorrect-title"
+      ]
+    ],
+    [
+      [
+        "question4_answer_incorrect",
+        "question4_answer_correct",
+        "question4_answer_incorrect"
+      ],
+      [
+        "question4_answer_incorrect_title",
+        "question4_answer_correct_title",
+        "question4_answer_incorrect-title"
+      ]
+    ],
+    [
+      [
+        "question5_answer_incorrect",
+        "question5_answer_correct",
+        "question5_answer_incorrect"
+      ],
+      [
+        "question5_answer_incorrect_title",
+        "question5_answer_correct_title",
+        "question5_answer_incorrect-title"
+      ],
+    ],
+    [
+      [
+       "question6_answer_incorrect",
+       "question6_answer_correct",
+       "question6_answer_incorrect"
+      ],
+      [
+        "question6_answer_incorrect_title",
+        "question6_answer_correct_title",
+        "question6_answer_incorrect-title"
+      ],
+    ],
+  ]
+
+
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -178,18 +241,18 @@ for (let i = 0; i < quizSetLength; i++) {
   }
   questionContent +=
     `</ul>` +
-    `<div class="js-answer${i + 1}_box">` +
-    `<p class="question${i + 1}_answer_correct_title" id="js-answer${
+    `<div class="js_answer${i + 1}_box">` +
+    `<p class="question${i + 1}_answer_correct_title" id="js_answer${
       i + 1
     }_correct_title">` +
     `${quizSet[i][2][index]}</p>` +
     ` <p class="question${i + 1}_answer_incorrect_title" ` +
-    `id="js-answer${i + 1}_incorrect_title">${quizSet[i][2][indexin]}</p>` +
+    `id="js_answer${i + 1}_incorrect_title">${quizSet[i][2][indexin]}</p>` +
     `<p class="question${i + 1}_answer_correct_content" ` +
     `id="js_answer${i + 1}_title_correct_content">` +
-    // ここをif (正解が押された時){<p class="question${i + 1}_answer_correct_title" id="js-answer${i + 1 }_correct_title">` +`正解！</p>}else... ってやれば上手くいく？？？
+    // ここをif (正解が押された時){<p class="question${i + 1}_answer_correct_title" id="js_answer${i + 1 }_correct_title">` +`正解！</p>}else... ってやれば上手くいく？？？
     `<span class="question${i + 1}_answer_correct_content_label">A</span>` +
-    `<span class="js-answer_text">${quizSet[i][1][index]}</span>` +
+    `<span class="js_answer_text">${quizSet[i][1][index]}</span>` +
     //１問目の場合、 quizSet[i]で[日本〜]、[1]で選択肢、[index]で[選択肢の中の正解]を出した
     `</p>` +
     `</div>` +
@@ -199,125 +262,125 @@ for (let i = 0; i < quizSetLength; i++) {
     .getElementById("js_main")
     .insertAdjacentHTML("beforeend", questionContent);
 
-  for(let p = 0; p < 3; p++){
-    let btn = document.getElementById(`js_answer_${i + 1}${p + 1}`);
-    let box = document.getElementsByClassName(`"js_answer${i + 1}_box"`);
-    let title = document.getElementById(`"js_answer${i + 1}_incorrect_title"`);
-    let boxclass = function (el) {
-      el.classList.add(`${quizSet[i + 1][6][p + 1]}`);
-    };
-    btn.addEventListener("click", () => {
-      boxclass(box);
-    },
-    false
-    );
-    document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-      const targetnode = document.getElementById(`js_answer_${i + 1}${p + 1}`);
-      targetnode.style.border = "solid 5px #0071BC"
-      targetnode.style.backgroundColor = "#f6f6f6";
-    });
-    document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-      const targetnode = document.getElementById(`${quizSet[i + 1][6]}_title`);
-      targetnode.style.display = "block";
-    });
-    document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-      for(let q = 0; q < 3; q++){
-        document.getElementById(`js_answer_${i + 1}${q + 1}`).setAttribute("disabled", "true");
-      }
-    });
-  }
+  // for(let p = 0; p < 3; p++){
+  //   let btn = document.getElementById(`js_answer_${i + 1}${p + 1}`);
+  //   let box = document.getElementsByClassName(`"js_answer${i + 1}_box"`);
+  //   let title = document.getElementById(`"js_answer${i + 1}_incorrect_title"`);
+  //   let boxclass = function (el) {
+  //     el.classList.toggle("js_answer1_box");
+  //   };
+  //   btn.addEventListener("click", () => {
+  //     boxclass(box);
+  //   },
+  //   false
+  //   );
+  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
+  //     const targetnode = document.getElementById(`js_answer_${i + 1}${p + 1}`);
+  //     targetnode.style.border = "solid 5px #0071BC"
+  //     targetnode.style.backgroundColor = "#f6f6f6";
+  //   });
+  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
+  //     const targetnode = document.querySelector(`${quizAnswer[i + 1][1][p]}`);
+  //     targetnode.style.display = "block";
+  //   });
+  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
+  //     for(let q = 0; q < 3; q++){
+  //       document.getElementById(`js_answer_${i + 1}${q + 1}`).setAttribute("disabled", "true");
+  //     }
+  //   });
+  // }
 }
 
-// //ボタン変化まとめ
-// // 1-1のボタン変化
-// let btn11 = document.querySelector("#js_answer_11");
-// // クリックする要素を取得
-// let box1 = document.getElementsByClassName("js-answer1_box");
-// // クラスを追加する要素を取得
-// let title11 = document.querySelector("#js-answer1_incorrect_title");
-// // クラスを追加する要素を取得
-// let box11class = function (el) {
-//   el[0].classList.toggle("question1_answer_incorrect");
-// };
-// // クラスを追加・削除する動きを定義
-// btn11.addEventListener(
-//   "click",
-//   function () {
-//     box11class(box1);
-//   },
-//   false
-// );
-// // ”btn”がクリックされたら”box”に対してbox11classを実行
-// document.getElementById("js_answer_11").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js_answer_11");
-//   targetnode.style.border = "solid 5px #0071BC";
-//   targetnode.style.backgroundColor = "#f6f6f6";
-// });
-// // ↑解答の外枠の色を変えてる
-// document.getElementById("js_answer_11").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js-answer1_incorrect_title");
-//   targetnode.style.display = "block";
-// });
-// // 
-// document.getElementById("js_answer_11").addEventListener("click", () => {
-//   document.getElementById("js_answer_11").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_12").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_13").setAttribute("disabled", "true");
-// });
-// // ここは変えなくても良い
-// // 1-2のボタン変化
-// // 1-2押された時の正解表示
-// let btn12 = document.querySelector("#js_answer_12");
-// let title12 = document.querySelector("#js-answer1_correct_title");
-// let box12class = function (el) {
-//   el[0].classList.toggle("question1_answer_correct");
-// };
-// btn12.addEventListener(
-//   "click",
-//   function () {
-//     box12class(box1);
-//   },
-//   false
-// );
-// document.getElementById("js_answer_12").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js_answer_12");
-//   targetnode.style.border = "solid 5px #0071BC";
-//   targetnode.style.backgroundColor = "#f6f6f6";
-// });
-// document.getElementById("js_answer_12").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js-answer1_correct_title");
-//   targetnode.style.display = "block";
-// });
-// document.getElementById("js_answer_12").addEventListener("click", () => {
-//   document.getElementById("js_answer_11").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_12").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_13").setAttribute("disabled", "true");
-// });
-// // 1-3のボタン変化
-// let btn13 = document.querySelector("#js_answer_13");
-// let title13 = document.querySelector("#js-answer1_incorrect_title");
-// let box13class = function (el) {
-//   el[0].classList.toggle("question1_answer_incorrect");
-// };
-// btn13.addEventListener(
-//   "click",
-//   function () {
-//     box13class(box1);
-//   },
-//   false
-// );
-// document.getElementById("js_answer_13").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js_answer_13");
-//   targetnode.style.border = "solid 5px #0071BC";
-//   targetnode.style.backgroundColor = "#f6f6f6";
-// });
+//ボタン変化まとめ
+// 1-1のボタン変化
+let btn11 = document.querySelector("#js_answer_11");
+// クリックする要素を取得
+let box1 = document.getElementsByClassName("js_answer1_box");
+// クラスを追加する要素を取得
+let title11 = document.querySelector("#js_answer1_incorrect_title");
+// クラスを追加する要素を取得
+let box11class = function (el) {
+  el[0].classList.toggle("question1_answer_incorrect");
+};
+// クラスを追加・削除する動きを定義
+btn11.addEventListener(
+  "click",
+  function () {
+    box11class(box1);
+  },
+  false
+);
+// ”btn”がクリックされたら”box”に対してbox11classを実行
+document.getElementById("js_answer_11").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer_11");
+  targetnode.style.border = "solid 5px #0071BC";
+  targetnode.style.backgroundColor = "#f6f6f6";
+});
+// ↑解答の外枠の色を変えてる
+document.getElementById("js_answer_11").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer1_incorrect_title");
+  targetnode.style.display = "block";
+});
+// 
+document.getElementById("js_answer_11").addEventListener("click", () => {
+  document.getElementById("js_answer_11").setAttribute("disabled", "true");
+  document.getElementById("js_answer_12").setAttribute("disabled", "true");
+  document.getElementById("js_answer_13").setAttribute("disabled", "true");
+});
+// ここは変えなくても良い
+// 1-2のボタン変化
+// 1-2押された時の正解表示
+let btn12 = document.querySelector("#js_answer_12");
+let title12 = document.querySelector("#js_answer1_correct_title");
+let box12class = function (el) {
+  el[0].classList.toggle("question1_answer_correct");
+};
+btn12.addEventListener(
+  "click",
+  function () {
+    box12class(box1);
+  },
+  false
+);
+document.getElementById("js_answer_12").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer_12");
+  targetnode.style.border = "solid 5px #0071BC";
+  targetnode.style.backgroundColor = "#f6f6f6";
+});
+document.getElementById("js_answer_12").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer1_correct_title");
+  targetnode.style.display = "block";
+});
+document.getElementById("js_answer_12").addEventListener("click", () => {
+  document.getElementById("js_answer_11").setAttribute("disabled", "true");
+  document.getElementById("js_answer_12").setAttribute("disabled", "true");
+  document.getElementById("js_answer_13").setAttribute("disabled", "true");
+});
+// 1-3のボタン変化
+let btn13 = document.querySelector("#js_answer_13");
+let title13 = document.querySelector("#js_answer1_incorrect_title");
+let box13class = function (el) {
+  el[0].classList.toggle("question1_answer_incorrect");
+};
+btn13.addEventListener(
+  "click",
+  function () {
+    box13class(box1);
+  },
+  false
+);
+document.getElementById("js_answer_13").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer_13");
+  targetnode.style.border = "solid 5px #0071BC";
+  targetnode.style.backgroundColor = "#f6f6f6";
+});
 
-// document.getElementById("js_answer_13").addEventListener("click", () => {
-//   const targetnode = document.getElementById("js-answer1_incorrect_title");
-//   targetnode.style.display = "block";
-// });
-// document.getElementById("js_answer_13").addEventListener("click", () => {
-//   document.getElementById("js_answer_11").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_12").setAttribute("disabled", "true");
-//   document.getElementById("js_answer_13").setAttribute("disabled", "true");
-// });
+document.getElementById("js_answer_13").addEventListener("click", () => {
+  const targetnode = document.getElementById("js_answer1_incorrect_title");
+  targetnode.style.display = "block";
+});
+document.getElementById("js_answer_13").addEventListener("click", () => {
+  document.getElementById("js_answer_11").setAttribute("disabled", "true");
+  document.getElementById("js_answer_12").setAttribute("disabled", "true");
+  document.getElementById("js_answer_13").setAttribute("disabled", "true");
+});
