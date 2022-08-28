d39620c0da01aac67cd774477b9243423400299f
kazuki iwagi <k.keio12569@gmail.com>
2022-08-28 16:21:44 +0900

    js書き換える

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -15,284 +15,122 @@
 //     ”正解！”の文字をとってきた。これで解答に”正解！”って出したい
 //     "不正解..."の文字をとってきた。これで解答に”不正解！”って出したい。これ上のを使えばこれなくしてもできそうだけど、やり方わからんから、とりま放置
 // ]
-const quizSet = [
-  [
-    "日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？",
-    [
-        "約28万人",
-        "約79万人",
-        "約183万人"
-    ],
-    [
-      "不正解...",
-      "正解!",
-      "不正解..."
-    ],
-    "img-quiz01.png",
-    1,
-    0,
+// const quizSet1 = {
+//   questionName:[
+//     '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？'
+//   ],
+//   img:[
+//     "img-quiz01.png"
+//   ],
+//   answerName: [
+//     "約28万人","約79万人","約183万人"
+//   ],
+//   answerNote: [
+//     "経済産業省 2019年3月 - IT 人材需給に関する調査"
+//   ],
+// }
+
+// const quizSet2 = {
+//   questionName:[
+//     '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？'
+//   ],
+//   img:[
+//     "img-quiz02.png"
+//   ],
+//   answerName: [
+//     "INTECH","BIZZECH","X-TECH"
+//   ],
+// }
+
+// const quizSet3 = {
+//   questionName:[
+//     'IoTとは何の略でしょう？'
+//   ],
+//   img:[
+//     "img-quiz03.png"
+//   ],
+//   answerName: [
+//     "Internet of Things","Integrate into Technology","Information on Tool"
+//   ],
+// }
+
+// const quizSet4 = {
+//   questionName:[
+//     'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？'
+//   ],
+//   img:[
+//     "img-quiz04.png"
+//   ],
+//   answerName: [
+//     "Society5.0","CyPhy","SDGs"
+//   ],
+//   answerNote: [
+//     "Society5.0 - 科学技術政策 - 内閣府"
+//   ],
+// }
+
+// const quizSet5 = {
+//   questionName:[
+//     'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？'
+//   ],
+//   img:[
+//     "img-quiz05.png"
+//   ],
+//   answerName: [
+//     "Web3.0","NFT","メタバース"
+//   ],
+// }
+
+// const quizSet6 = {
+//   questionName:[
+//     '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？'
+//   ],
+//   img:[
+//     "img-quiz6.png"
+//   ],
+//   answerName: [
+//     "約２倍","約５倍","約１１倍"
+//   ],
+//   answerNote: [
+//     "Accenture Technology Vision 2021"
+//   ]
+// }
+
+const allQuiz = {
+  questionName: [
+    '日本のIT人材が2030年には最大どれくらい不足すると言われているでしょうか？',
+    '既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？',
+    'IoTとは何の略でしょう？',
+    'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+    'イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？',
+    '先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？'
   ],
-  [
-    "既存業界のビジネスと、先進的なテクノロジーを結びつけて生まれた、新しいビジネスのことをなんと言うでしょう？",
-    [
-      "INTECH",
-      "BIZZTECH",
-      "X-TECH"
-    ],
-    [
-      "不正解...",
-      "不正解...",
-      "正解!"
-    ],
-    "img-quiz02.png",
-    2,
-    0,
+  img: [
+    "img-quiz1.png",
+    "img-quiz2.png",
+    "img-quiz3.png",
+    "img-quiz4.png",
+    "img-quiz5.png",
+    "img-quiz6.png"
   ],
-  [
-    "IoTとは何の略でしょう？",
-    [
-      "Internet of Things", 
-      "Integrate into Technology", 
-      "Information of Tool"
-    ],
-    [
-      "正解!",
-      "不正解...",
-      "不正解..."
-    ],
-    "img-quiz03.png",
-    0,
-    1,
-  ],
-  [
-    "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
-    [
-      "Society 5.0",
-      "CyPhy",
-      "SDGs"
-    ],
-    [
-      "正解!",
-      "不正解...",
-      "不正解..."
-    ],
-    "img-quiz04.png",
-    0,
-    1,
-  ],
-  [
-    "イギリスのコンピューター科学者であるギャビン・ウッド氏が提唱した、ブロックチェーン技術を活用した「次世代分散型インターネット」のことをなんと言うでしょう？",
-    [
-      "Web3.0",
-      "NFT",
-      "メタバース"
-    ],
-    [
-      "正解!",
-      "不正解...",
-      "不正解..."
-    ],
-     "img-quiz05.png",
-    //  ここに , を入れ忘れて画像が読み込めなかった！！  注意！！！！！
-    0,
-    1,
-  ],
-  [
-    "先進テクノロジー活用企業と出遅れた企業の収益性の差はどれくらいあると言われているでしょうか？",
-    [
-      "約２倍",
-      "約５倍",
-      "約１１倍"
-    ],
-    [
-      "不正解...",
-      "正解!",
-      "不正解..."
-    ],
-    "img-quiz06.png",
-    1,
-    0,
-  ],
+  // 画像、番号変わるだけだから、ここをfor文で回せばいい感じになる気がする。まぁ、forでいい感じにいけたら、ここ消す！！
+  answerName: [
+     [
+      "約28万人","約79万人","約183万人"
+     ],
+     [
+      "INTECH","BIZZECH","X-TECH"
+     ],
+     [
+      "Internet of Things","Integrate into Technology","Information on Tool"
+     ],
+     [
+      "Society5.0 - 科学技術政策 - 内閣府"
+     ]
   ]
-  // const quizAnswer = [
-  //  [
-  //   js_answer[問題番号]_boxにクラスをつける
-  //  ]
-  //  [
-  //   js_answer
-  //  ]
-  // ]
-  const quizAnswer = [
-    [
-      [
-        "question1_answer_incorrect",
-        "question1_answer_correct",
-        "question1_answer_incorrect"
-      ],
-      [
-        "question1_answer_incorrect_title",
-        "question1_answer_correct_title",
-        "question1_answer_incorrect-title"
-      ]
-    ],
-    [
-      [
-        "question2_answer_incorrect",
-        "question2_answer_correct",
-        "question2_answer_incorrect"
-      ],
-      [
-        "question2_answer_incorrect_title",
-        "question2_answer_correct_title",
-        "question2_answer_incorrect-title"
-      ]
-    ],
-    [
-      [
-        "question3_answer_incorrect",
-        "question3_answer_correct",
-        "question3_answer_incorrect"
-      ],
-      [
-        "question3_answer_incorrect_title",
-        "question3_answer_correct_title",
-        "question3_answer_incorrect-title"
-      ]
-    ],
-    [
-      [
-        "question4_answer_incorrect",
-        "question4_answer_correct",
-        "question4_answer_incorrect"
-      ],
-      [
-        "question4_answer_incorrect_title",
-        "question4_answer_correct_title",
-        "question4_answer_incorrect-title"
-      ]
-    ],
-    [
-      [
-        "question5_answer_incorrect",
-        "question5_answer_correct",
-        "question5_answer_incorrect"
-      ],
-      [
-        "question5_answer_incorrect_title",
-        "question5_answer_correct_title",
-        "question5_answer_incorrect-title"
-      ],
-    ],
-    [
-      [
-       "question6_answer_incorrect",
-       "question6_answer_correct",
-       "question6_answer_incorrect"
-      ],
-      [
-        "question6_answer_incorrect_title",
-        "question6_answer_correct_title",
-        "question6_answer_incorrect-title"
-      ],
-    ],
-  ]
-
-
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-};
-shuffleArray(quizSet);
-
-let quizSetLength = quizSet.length;
-for (let i = 0; i < quizSetLength; i++) {
-  let index = quizSet[i][4];
-  let indexin = quizSet[i][5];
-  let questionContent =
-    `<section class="question${i + 1}">` +
-    `<div class="question${i + 1}_quiz">` +
-    `<h2 class="question${i + 1}_quiz_title">` +
-    `<span class="question${i + 1}_quiz_title_label">Q${i + 1}</span>` +
-    `<span class="question${i + 1}_quiz_title_text">${quizSet[i][0]}</span>` +
-    `</h2>` +
-    `<figure class="question${i + 1}_quiz_title_image">` +
-    `<img src="${quizSet[i][3]}" alt="" class="question${
-      i + 1
-    }_quiz_title_img">` +
-    ` </figure>` +
-    `</div>` +
-    `<div class="question${i + 1}_answer">` +
-    `<span class="question${i + 1}_answer_label">A</span>` +
-    `<ul class="question${i + 1}_answer_list">`;
-  for (let j = 0; j < quizSet[i][1].length; j++) {
-    questionContent +=
-      `<li class="question${i + 1}_answer_list_item">` +
-      `<button class="question${i + 1}_answer_list_item_button" id="js_answer_${
-        i + 1
-      }${j + 1}">${quizSet[i][1][j]}` +
-      ` <img src="icon-arrow.svg" ` +
-      `class="question${i + 1}_answer_list_item_button_icon" ></img>` +
-      `</button>` +
-      `</li>`;
-  }
-  questionContent +=
-    `</ul>` +
-    `<div class="js_answer${i + 1}_box">` +
-    `<p class="question${i + 1}_answer_correct_title" id="js_answer${
-      i + 1
-    }_correct_title">` +
-    `"正解！"</p>` +
-    ` <p class="question${i + 1}_answer_incorrect_title" ` +
-    `id="js_answer${i + 1}_incorrect_title">"不正解..."</p>` +
-    `<p class="question${i + 1}_answer_correct_content" ` +
-    `id="js_answer${i + 1}_title_correct_content">` +
-    // ここをif (正解が押された時){<p class="question${i + 1}_answer_correct_title" id="js_answer${i + 1 }_correct_title">` +`正解！</p>}else... ってやれば上手くいく？？？
-    `<span class="question${i + 1}_answer_correct_content_label">A</span>` +
-    `<span class="js_answer_text">${quizSet[i][1][index]}</span>` +
-    //１問目の場合、 quizSet[i]で[日本〜]、[1]で選択肢、[index]で[選択肢の中の正解]を出した
-    `</p>` +
-    `</div>` +
-    `</div>` +
-    `</section>`;
-  document
-    .getElementById("js_main")
-    .insertAdjacentHTML("beforeend", questionContent);
-
-  // for(let p = 0; p < 3; p++){
-  //   let btn = document.getElementById(`js_answer_${i + 1}${p + 1}`);
-  //   let box = document.getElementsByClassName(`"js_answer${i + 1}_box"`);
-  //   let title = document.getElementById(`"js_answer${i + 1}_incorrect_title"`);
-  //   let boxclass = function (el) {
-  //     el.classList.toggle("js_answer1_box");
-  //   };
-  //   btn.addEventListener("click", () => {
-  //     boxclass(box);
-  //   },
-  //   false
-  //   );
-  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-  //     const targetnode = document.getElementById(`js_answer_${i + 1}${p + 1}`);
-  //     targetnode.style.border = "solid 5px #0071BC"
-  //     targetnode.style.backgroundColor = "#f6f6f6";
-  //   });
-  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-  //     const targetnode = document.querySelector(`${quizAnswer[i + 1][1][p]}`);
-  //     targetnode.style.display = "block";
-  //   });
-  //   document.getElementById(`js_answer_${i + 1}${p + 1}`).addEventListener("click", () => {
-  //     for(let q = 0; q < 3; q++){
-  //       document.getElementById(`js_answer_${i + 1}${q + 1}`).setAttribute("disabled", "true");
-  //     }
-  //   });
-  // }
 }
 
-//ボタン変化まとめ
-// 1-1のボタン変化
+
 let btn11 = document.querySelector("#js_answer_11");
 // クリックする要素を取得
 let box1 = document.getElementsByClassName("js_answer1_box");
@@ -384,3 +222,6 @@ document.getElementById("js_answer_13").addEventListener("click", () => {
   document.getElementById("js_answer_12").setAttribute("disabled", "true");
   document.getElementById("js_answer_13").setAttribute("disabled", "true");
 });
+
+
+
