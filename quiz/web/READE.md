2770814d9f218c45a259bd2eeff8bea211eec05f
kazuki1023 <k.keio12569@gmail.com>
2022-10-19 20:37:08 +0900

    学習時間おしたら、カレンダー出てくる。閉じるボタン押したら閉じる

---
+++ quiz/web/READE.md
@@ -0,0 +1,8 @@
+# chexkboxの作り方メモ
+checkoboxを普通に作っとく（灰色で、押す前にすでにチェックボックスがある状態にしとく）
+この時、input tyoe="checkbox"で作っておく
+
+↓
+addeventlisnerでクラス付与、そのクラスにaccent-colorで色（青）をチェックボックスに付ける。
+
+##　わからなかったので飛ばす　10/19
--- quiz/web/script/web.js
+++ quiz/web/script/web.js
@@ -1,12 +1,28 @@
 "use strict"
+// 記録ボタンを押すとモーダルが出てくる
 const headerRecordButton = document.getElementById('header_record_button');
 const modal = document.getElementsByClassName('modal');
 headerRecordButton.addEventListener("click", () => {
   modal[0].classList.toggle("modal_active");
 });
 
-const modalcloseButton = document.getElementsByClassName('modal_close');
-modalcloseButton[0].addEventListener("click", () => {
+// ボタンを押すと、モーダルが閉じる処理
+const modalCloseButton = document.getElementsByClassName('modal_close');
+modalCloseButton[0].addEventListener("click", () => {
   modal[0].classList.remove("modal_active");
 })
 
+// 学習日を押すとカレンダーが出てくる
+const learningDayDetail = document.getElementById('learning_day_detail');
+const calender = document.getElementsByClassName("calender");
+learningDayDetail.addEventListener("click", () => {
+  calender[0].classList.toggle("calender_active");
+  modal[0].classList.remove("modal_active");
+})
+
+//ボタンを押すとカレンダーが閉じる
+const calenderCloseButton = document.getElementById("calender_close");
+calenderCloseButton.addEventListener("click", () => {
+  calender[0].classList.remove("calender_active");
+  modal[0].classList.toggle("modal_active");
+})
